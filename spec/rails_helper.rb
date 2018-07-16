c8a22ab0b9549f4362195dbc6109f859244d82ca
Gianluca Rizzo <gianluca.rizzo@keypartner.it>
2018-07-16 21:51:19 +0200

    Remove knapsack

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,3 @@
-require 'knapsack_pro'
-KnapsackPro::Adapters::RSpecAdapter.bind
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
